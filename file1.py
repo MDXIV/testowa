ab430dde3b02af7d80c018538af619ca183a93df
Dominik <Dominik@DESKTOP-A58BUDQ>
2021-01-19 08:26:47 +0100

    trzeci

--- file1.py
+++ file1.py
@@ -1,3 +1,4 @@
 print("plik 1 linia 1")
 print("plik 1 linia 2")
 print("plik 1 linia 3")
+print("plik 1 linia 4")
--- file2.py
+++ file2.py
@@ -1,3 +1,4 @@
 print("plik 2 linia 1")
 print("plik 2 linia 2")
 print("plik 2 linia 3")
+print("plik 2 linia 4")
