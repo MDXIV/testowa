67fadb504fa8ffa3daadbcb5c25ee2e45544e4cf
Dominik <Dominik@DESKTOP-A58BUDQ>
2021-01-19 09:19:07 +0100

    rd

--- file2.py
+++ file2.py
@@ -2,8 +2,4 @@ print("plik 2 linia 1")
 print("plik 2 linia 2")
 print("plik 2 linia 3")
 print("plik 2 linia 4")
-<<<<<<< HEAD
-print("plik test2 linia teset2")
-=======
 print("plik test linia test XD")
->>>>>>> 184daa43893de02f65992604b65f859434e963ca
